5b10825a4027c573a74f35b6e8e275a39d2f7574
Alex Perronnet <aperronnet.login@gmail.com>
2022-12-11 00:48:38 +0100

    Setup project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import '@/styles/main.scss'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const container = document.getElementById('root')
+const root = ReactDOM.createRoot(container)
+
+root.render(<React.StrictMode>Ceci est une phrase de test</React.StrictMode>)
