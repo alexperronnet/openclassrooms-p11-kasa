4ae213d0e70b1ee43bf188754303a141e4f98aff
Alex Perronnet <aperronnet.login@gmail.com>
2022-12-11 05:07:27 +0100

    Fix accessibility

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,7 +5,7 @@ export default function notFound() {
     <div className="not-found">
       <div className="not-found__infos">
         <h1 className="not-found__title">404</h1>
-        <p className="not-found__text">Oups! La page que vous demandez n'existe pas...</p>
+        <p className="not-found__text">Oups ! La page que vous demandez n'existe pas...</p>
       </div>
       <Link to="/" className="not-found__link">
         Retourner sur la page d’accueil
