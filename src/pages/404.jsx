04077687a2fb1c9c24edbb90c07544315921084f
Alex Perronnet <aperronnet.login@gmail.com>
2022-12-11 05:01:49 +0100

    Create not found page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,3 +1,15 @@
+import { Link } from 'react-router-dom'
+
 export default function notFound() {
-  return <h1>404</h1>
+  return (
+    <div className="not-found">
+      <div className="not-found__infos">
+        <h1 className="not-found__title">404</h1>
+        <p className="not-found__text">Oups! La page que vous demandez n'existe pas...</p>
+      </div>
+      <Link to="/" className="not-found__link">
+        Retourner sur la page d’accueil
+      </Link>
+    </div>
+  )
 }
