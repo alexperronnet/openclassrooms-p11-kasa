f66f8bac8fb1a2f586cb10c4dfa40ed81dfa155b
Alex Perronnet <aperronnet.login@gmail.com>
2022-12-11 04:41:06 +0100

    Build layout

--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -1,3 +1,14 @@
+import { Logo } from '@/components'
+
 export default function Footer() {
-  return <footer></footer>
+  const currentYear = new Date().getFullYear()
+
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+        <Logo className="footer__logo" />
+        <p className="footer__copyright">&#169; {currentYear} Kasa. Tous droits réservés.</p>
+      </div>
+    </footer>
+  )
 }
--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,3 +1,18 @@
+import { NavLink } from 'react-router-dom'
+import { Logo } from '@/components'
+
 export default function Header() {
-  return <header></header>
+  return (
+    <header className="header">
+      <Logo className="header__logo" />
+      <nav className="header__nav">
+        <NavLink to="/" className="header__nav-link">
+          Accueil
+        </NavLink>
+        <NavLink to="/a-propos" className="header__nav-link">
+          À Propos
+        </NavLink>
+      </nav>
+    </header>
+  )
 }
