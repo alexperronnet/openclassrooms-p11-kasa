f77c1cc820823fba0c710463c540773b70b1733d
Alex Perronnet <aperronnet.login@gmail.com>
2022-12-16 06:35:26 +0100

    Minor update

--- src/components/rental-card.jsx
+++ src/components/rental-card.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export default function RentalCard({ data }) {
   return (
     <Link to={`/location/${data.id}`} className="rental-card">
-      <img className="rental-card__image" src={data.cover} alt={data.title} />
+      <img className="rental-card__image" src={data.cover} alt={data.title} loading="lazy" />
       <h2 className="rental-card__title">{data.title}</h2>
     </Link>
   )
